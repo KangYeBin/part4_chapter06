370dc8fbdcc0aa8cac1d133dccccf6a87755a77b
KangYeBin <yb725v@naver.com>
2022-06-10 01:52:24 +0900

    03. 근접 측정소 정보 불러오기

---
+++ app/src/main/java/com/yb/part4_chapter06/data/Repository.kt
@@ -0,0 +1,44 @@
+package com.yb.part4_chapter06.data
+
+import com.google.gson.GsonBuilder
+import com.yb.part4_chapter06.BuildConfig
+import com.yb.part4_chapter06.data.services.KakaoLocalApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object Repository {
+
+    suspend fun getNearByMonitoringStation(latitude: Double, longitude: Double) {
+        val tmCoordinates = kakaoLacalApiService.getTmCoordinates(longitude, latitude)
+            .body()
+            ?.documents
+            ?.firstOrNull()
+
+        val tmX = tmCoordinates?.x
+        val tmY = tmCoordinates?.y
+    }
+
+    private val kakaoLacalApiService: KakaoLocalApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(Url.KAKAO_API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(buildHttpClient())
+            .build()
+            .create()
+    }
+
+    private fun buildHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+            }).build()
+
+
+}
---
+++ app/src/main/java/com/yb/part4_chapter06/data/Url.kt
@@ -0,0 +1,5 @@
+package com.yb.part4_chapter06.data
+
+object Url {
+    const val KAKAO_API_BASE_URL = "https://dapi.kakao.com/"
+}
