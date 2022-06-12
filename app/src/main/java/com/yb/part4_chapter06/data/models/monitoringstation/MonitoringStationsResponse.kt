8d00c096a1ecf371d25cf1cd32a760dba687005b
KangYeBin <yb725v@naver.com>
2022-06-12 15:06:26 +0900

    04. 근접 측정소 정보 불러오기 - 2

---
+++ app/src/main/java/com/yb/part4_chapter06/data/models/monitoringstation/MonitoringStationsResponse.kt
@@ -0,0 +1,9 @@
+package com.yb.part4_chapter06.data.models.monitoringstation
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MonitoringStationsResponse(
+    @SerializedName("response")
+    val response: Response?
+)
